55ef128a7ac17862ac65fd7d6bf174c48dc8dce4
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2023-09-14 19:49:42 +0200

    Adding fedora install script

---
+++ install/fedora.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo dnf install -y \
+    i3 \
+    rxvt-unicode \
+    fish \
+    ImageMagick \
+    xautolock \
+    xrandr \
+    rofi \
+    dejavu-fonts-all \
+    fontawesome-fonts
