6dd8f632ad61b0a668837fbeab0456c1cb30dba8
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-04-03 20:37:45 +0200

    symlink.sh: link scripts

--- symlink.sh
+++ symlink.sh
@@ -16,5 +16,9 @@ for target in ${DIRECTORIES[@]}; do
 	ln -sfr -t $HOME/.config $BASE_PATH/$target
 done
 
-ln -sfr $BASE_PATH/tmux.conf $HOME/.tmux.conf 
-ln -sfr $BASE_PATH/Xresources $HOME/.Xresources 
+ln -sfr $BASE_PATH/tmux.conf $HOME/.tmux.conf
+ln -sfr $BASE_PATH/Xresources $HOME/.Xresources
+
+for script in $(find $BASE_PATH/scripts -type f); do
+	ln -sfr $script $HOME/bin/$(basename $script)
+done
