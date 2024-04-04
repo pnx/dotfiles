325b0856e868bb07caaaeffd0713da030aeb9f50
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-04-04 23:00:49 +0200

    nvim: lsp: make gopls fix those damn unused imports when iam saving the files

--- nvim/lua/plugins/ide/lsp.lua
+++ nvim/lua/plugins/ide/lsp.lua
@@ -24,7 +24,21 @@ return {
 				}
 			},
 			gopls = {
-				format_on_save = true,
+				-- format_on_save = true,
+				on_save = function ()
+					local params = vim.lsp.util.make_range_params()
+					params.context = {only = {"source.organizeImports"}}
+					local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 1000)
+					for cid, res in pairs(result or {}) do
+						for _, r in pairs(res.result or {}) do
+							if r.edit then
+								local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+								vim.lsp.util.apply_workspace_edit(r.edit, enc)
+							end
+						end
+					end
+					vim.lsp.buf.format({async = false})
+				end,
 				settings = {
 					gopls = {
 						analyses = {
@@ -89,6 +103,14 @@ return {
 						callback = function()
 							vim.lsp.buf.format()
 						end,
+					})
+				end
+
+				if server_opt.on_save then
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = server_opt.on_save,
 					})
 				end
 
