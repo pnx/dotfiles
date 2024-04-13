addd768bc39de1e830bffa448a45de63a277e444
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-04-13 14:43:31 +0200

    fish: update prompt

--- fish/functions/prompt_login.fish
+++ fish/functions/prompt_login.fish
@@ -21,8 +21,6 @@ function prompt_login --description 'display user name for the prompt'
     # If we're running via SSH, change the host color.
     set -l color_host $fish_color_host
     if set -q SSH_TTY; and set -q fish_color_host_remote
-        set color_host $fish_color_host_remote
+        echo -n -s (set_color $fish_color_user) "$USER" (set_color normal) / (set_color $color_host) (prompt_hostname) (set_color normal)
     end
-
-    echo -n -s (set_color $fish_color_user) "$USER" (set_color normal) / (set_color $color_host) (prompt_hostname) (set_color normal)
 end
