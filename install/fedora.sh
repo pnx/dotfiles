e5653ccc104c4cf8f2f5447604521b5f5150d088
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-04-15 16:04:12 +0200

    install/fedora.sh: split packages into sections

--- install/fedora.sh
+++ install/fedora.sh
@@ -1,16 +1,47 @@
 #!/bin/bash
 
-sudo dnf install -y \
-    xorg-x11-xinit \
-    i3 \
-    lxappearance \
-    rxvt-unicode \
-    kitty \
-    fish \
-    ImageMagick \
-    xautolock \
-    xrandr \
-    arandr \
-    rofi \
-    dejavu-fonts-all \
-    fontawesome-fonts
+if [ $# -lt 1 ]; then
+    SELECTED=(core i3)
+else 
+    SELECTED=$@
+fi
+
+for sel in "${SELECTED[@]}"; do
+    case $sel in
+        "core")
+            sudo dnf install -y \
+                xsel \
+                fish \
+                kitty \
+                tmux \
+                xsel \
+                fzf \
+                fswatch
+            ;;
+        "nvim")
+            sudo dnf install -y \
+                ripgrep \
+                fswatch \
+                fd-find \
+                neovim
+            ;;
+        "i3")
+            sudo dnf install -y \
+                xorg-x11-xinit \
+                i3 \
+                lxappearance \
+                ImageMagick \
+                xautolock \
+                xrandr \
+                arandr \
+                rofi \
+                dejavu-fonts-all \
+                fontawesome-fonts
+            ;;
+        *)
+            echo "Unknown option: $sel"
+            exit 1
+            ;;
+    esac
+done
+
