a40dec65959ea9624887f7aa7740b36d8ac43565
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-06-08 16:50:43 +0200

    install.sh: format fixes

--- install.sh
+++ install.sh
@@ -3,18 +3,18 @@
 BASE_PATH=$(dirname $(readlink -f $BASH_SOURCE))
 
 DIRECTORIES=(
-	nvim
-	fish
-	alacritty
-	i3
-	i3status
-	rofi
-	smartgit
+    nvim
+    fish
+    alacritty
+    i3
+    i3status
+    rofi
+    smartgit
     lazygit
 )
 
 for target in ${DIRECTORIES[@]}; do
-	ln -sfr -t $HOME/.config $BASE_PATH/$target
+    ln -sfr -t $HOME/.config $BASE_PATH/$target
 done
 
 ln -sfr $BASE_PATH/tmux.conf $HOME/.tmux.conf
@@ -22,5 +22,5 @@ ln -sfr $BASE_PATH/Xresources $HOME/.Xresources
 
 mkdir -p $HOME/bin
 for script in $(find $BASE_PATH/scripts -type f); do
-	ln -sfr $script $HOME/bin/$(basename $script)
+    ln -sfr $script $HOME/bin/$(basename $script)
 done
