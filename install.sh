410258536398b58551615f6a82d52b7a30968c32
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-06-23 17:25:09 +0200

    adding bashrc

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ for target in ${DIRECTORIES[@]}; do
     ln -sfr -t $HOME/.config $BASE_PATH/$target
 done
 
+ln -sfr $BASE_PATH/bashrc $HOME/.bashrc
 ln -sfr $BASE_PATH/tmux.conf $HOME/.tmux.conf
 ln -sfr $BASE_PATH/Xresources $HOME/.Xresources
 
