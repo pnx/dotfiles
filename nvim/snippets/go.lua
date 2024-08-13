c8637d1aeed2219b65b21be6032e07ee5705b0a5
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-08-13 15:31:39 +0200

    nvim/snippets/go.lua: add snippet for if error case

--- nvim/snippets/go.lua
+++ nvim/snippets/go.lua
@@ -5,6 +5,12 @@ local t = ls.text_node
 local i = ls.insert_node
 
 return {
+    s("iferr", {
+        t({"if err != nil {", ""}),
+        t({"\treturn err", ""}),
+        t({"}"})
+    }),
+
     -- Test function
     s("tst", {
         t("func Test"), i(1, "Module"), t("_"), i(2, "Name"), t({"(t *testing.T) {", ""}),
