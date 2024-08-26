388916ee137ec6ee33d5d05f7c5d77cbbdbf22fb
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-08-26 19:24:17 +0200

    nvim: utils.misc: Adding luadoc comment for opts table passed to highlight_yank()

--- nvim/lua/user/utils/misc.lua
+++ nvim/lua/user/utils/misc.lua
@@ -1,6 +1,12 @@
 
 local M = {}
 
+---@class HighlightYankConfig
+---@field enable boolean
+---@field timeout number
+---@field higroup string
+
+---@param opts HighlightYankConfig
 M.highlight_yank = function(opts)
     vim.api.nvim_create_autocmd("TextYankPost", {
         group = vim.api.nvim_create_augroup("highlight_yank", {}),
