88454b931c3d2d3abde32280024cf5c180129304
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-08-26 18:46:56 +0200

    nvim add a files with type aliases because luals is being a twat

---
+++ nvim/_types.lua
@@ -0,0 +1,14 @@
+
+--- LuaLS is weird and does not recognize builtin types.
+--- even tho it should. So define them as alias
+---@alias nil nil
+---@alias any any
+---@alias boolean boolean
+---@alias string string
+---@alias number number
+---@alias integer integer
+---@alias function function
+---@alias table table
+---@alias thread thread
+---@alias userdata userdata
+---@alias lightuserdata lightuserdata
