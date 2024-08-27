1e7160dbbee0ce439e2cd63cd48cf384dbb45a93
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-08-27 22:13:35 +0200

    nvim: options: add folds to status column

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -56,7 +56,7 @@ vim.o.relativenumber = true
 vim.o.numberwidth = 5
 
 -- Gutter format
-vim.o.statuscolumn = '%s %=%{v:relnum?v:relnum:v:lnum} │ '
+vim.o.statuscolumn = '%s %=%{v:relnum?v:relnum:v:lnum} %C│ '
 
 -- search
 vim.o.hlsearch = false
