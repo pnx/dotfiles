25599c77b21a3572184c74a3c43ec574e849d7f2
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-09-01 14:31:09 +0200

    nvim/lua/user/extras.lua: create Format command.

--- nvim/lua/user/extras.lua
+++ nvim/lua/user/extras.lua
@@ -3,3 +3,20 @@
 if user.highlight_yank.enable or false then
     require('user.utils.misc').highlight_yank(user.highlight_yank)
 end
+
+-- Default formatter. will select the whole file and use formatexpr to format the file.
+-- this is slow and ugly, but works.
+if user.formatter == nil then
+    user.formatter = [[mfgggqG`f=zz]]
+end
+
+vim.api.nvim_create_user_command('Format', function()
+    if type(user.formatter) == "string" then
+        local cmd = vim.api.nvim_replace_termcodes(user.formatter, true, false, true)
+        vim.api.nvim_feedkeys(cmd, 'n', false)
+    elseif type(user.formatter) == "function" then
+        user.formatter()
+    else
+        vim.api.nvim_err_writeln("No formatter found")
+    end
+end, {})
