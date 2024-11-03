f63bdaa84fb995de409396290206f84cbf083887
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-11-03 13:48:40 +0100

    nvim: php: remove phpactor plugin

--- nvim/lua/user/plugins/lang/php.lua
+++ nvim/lua/user/plugins/lang/php.lua
@@ -85,11 +85,6 @@ return {
             }
         }
     },
-    {
-        'phpactor/phpactor',
-        build = 'composer install --no-dev --optimize-autoloader',
-        ft = 'php',
-    },
     -- Linting
     {
         'mfussenegger/nvim-lint',
