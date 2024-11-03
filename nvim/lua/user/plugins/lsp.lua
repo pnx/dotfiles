401e6f2ed0776237f9eca9ce7e3f8ace0d070e55
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-11-03 13:42:27 +0100

    nvim: adding ray-x/lsp_signature.nvim

--- nvim/lua/user/plugins/lsp.lua
+++ nvim/lua/user/plugins/lsp.lua
@@ -18,6 +18,17 @@ return {
                 "hrsh7th/cmp-nvim-lsp-signature-help"
             },
         },
+        {
+            "ray-x/lsp_signature.nvim",
+            event = "VeryLazy",
+            opts = {
+                hint_enable = false,
+                fix_pos = true,
+                handler_opts = {
+                    border = "none"
+                }
+            },
+        },
         -- LSP often return markdown that neovim parses.
         -- make sure we have a plugin that can render markdown to nicer text
         {
