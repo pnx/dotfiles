01b72ed9a791b69231fe0883035a7616a3f11e34
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-11-03 13:48:16 +0100

    nvim: php: configure both phpactor (for refactoring) and intelephense (for everything else) LSP servers

--- nvim/lua/user/plugins/lang/php.lua
+++ nvim/lua/user/plugins/lang/php.lua
@@ -1,9 +1,58 @@
 local lspservers = {
-    -- phpactor = {
-    --     inlay_hints = {
-    --         enabled = false
-    --     }
-    -- },
+    --- @type LSPServerOptions
+    phpactor = {
+        inlay_hints = {
+            enabled = false
+        },
+        codelens = {
+            enabled = false
+        },
+        document_highlight = {
+            enabled = false
+        },
+        diagnostics = false,
+        hover = false,
+        definition = false,
+        init_options = {
+            -- Only interested in code actions. Disable everything else!
+            ["completion_worse.completor.doctrine_annotation.enabled"] = false,
+            ["completion_worse.completor.imported_names.enabled"] = false,
+            ["completion_worse.completor.worse_parameter.enabled"] = false,
+            ["completion_worse.completor.named_parameter.enabled"] = false,
+            ["completion_worse.completor.constructor.enabled"] = false,
+            ["completion_worse.completor.class_member.enabled"] = false,
+            ["completion_worse.completor.scf_class.enabled"] = false,
+            ["completion_worse.completor.local_variable.enabled"] = false,
+            ["completion_worse.completor.subscript.enabled"] = false,
+            ["completion_worse.completor.declared_function.enabled"] = false,
+            ["completion_worse.completor.declared_constant.enabled"] = false,
+            ["completion_worse.completor.declared_class.enabled"] = false,
+            ["completion_worse.completor.expression_name_search.enabled"] = false,
+            ["completion_worse.completor.use.enabled"] = false,
+            ["completion_worse.completor.class_like.enabled"] = false,
+            ["completion_worse.completor.type.enabled"] = false,
+            ["completion_worse.completor.keyword.enabled"] = false,
+            ["completion_worse.completor.docblock.enabled"] = false,
+            ["completion_worse.completor.attribute.enabled"] = false,
+            ["completion_worse.completor.constant.enabled"] = false,
+            ["completion_worse.snippets"] = false,
+            ["completion.limit"] = 0,
+            ["language_server.diagnostics_on_open"] = false,
+            ["language_server.diagnostics_on_save"] = false,
+            ["language_server.diagnostics_on_update"] = false,
+            ["language_server_worse_reflection.diagnostics.enable"] = false,
+            ["language_server_worse_reflection.inlay_hints.enable"] = false,
+            ["language_server_php_cs_fixer.enabled"] = false,
+            ["language_server_phpstan.enabled"] = false,
+            ["language_server_psalm.enabled"] = false,
+            ["code_transform.refactor.generate_accessor.prefix"] = "get",
+            ["code_transform.refactor.generate_accessor.upper_case_first"] = true,
+            ["code_transform.refactor.generate_mutator.prefix"] = "set",
+            ["code_transform.refactor.generate_mutator.upper_case_first"] = true,
+            ["code_transform.refactor.generate_mutator.fluent"] = true,
+        }
+    },
+    -- Use intelephense for as main server.
     intelephense = {}
 }
 
