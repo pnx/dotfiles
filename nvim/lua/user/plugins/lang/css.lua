6707c863bb45d49ce54e2fe3ec241ca9684af97d
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2024-11-11 08:48:09 +0100

    nvim: css: enable tailwindcss lsp

--- nvim/lua/user/plugins/lang/css.lua
+++ nvim/lua/user/plugins/lang/css.lua
@@ -9,7 +9,7 @@ return {
         "neovim/nvim-lspconfig",
         opts = {
             servers = {
-                -- tailwindcss = {},
+                tailwindcss = {},
             }
         },
     }
