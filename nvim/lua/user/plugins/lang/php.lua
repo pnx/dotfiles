c4155370b4367dbfdb2dda6eaa22375b207dc964
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-01-19 00:16:42 +0100

    nvim/lua/user/plugins/lang/php.lua: ensure blade treesitter parser is installed.

--- nvim/lua/user/plugins/lang/php.lua
+++ nvim/lua/user/plugins/lang/php.lua
@@ -73,7 +73,7 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         opts = {
-            ensure_installed = { "php", "php_only", "phpdoc", "blade" }
+            ensure_installed = { "php", "php_only", "phpdoc" }
         }
     },
     {
