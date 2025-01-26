fffa6d53220594d47909d75749b734be073d890e
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-01-26 18:45:46 +0100

    nvim/queries/php_only/indents.scm: fix phpdoc indents

--- nvim/queries/php_only/indents.scm
+++ nvim/queries/php_only/indents.scm
@@ -3,8 +3,5 @@
 ; Fix for phpdoc comments that don't align * properly.
 ; This is black magic. I came up with it myself but
 ; i don't know why it works.
-;(ERROR
-; "/"
-; "**"
-; @indent.align)
+(ERROR "/" "**" "*" @indent.align)
 
