5a86fd971c84a99769d558ae7895e742a1446d0f
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-06-13 11:44:26 +0200

    keymaps/install.sh: add BASE_PATH

--- keymaps/install.sh
+++ keymaps/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+BASE_PATH=$(dirname $(readlink -f $BASH_SOURCE))
 
-cat pnx-us-se.map | gzip | sudo tee /usr/share/kbd/keymaps/i386/qwerty/pnx-us-se.map.gz > /dev/null
-sudo install pnx-us-se /usr/share/X11/xkb/symbols
+cat $BASE_PATH/pnx-us-se.map | gzip | sudo tee /usr/share/kbd/keymaps/i386/qwerty/pnx-us-se.map.gz > /dev/null
+sudo install $BASE_PATH/pnx-us-se /usr/share/X11/xkb/symbols
 
 sudo localectl --no-convert set-keymap pnx-us-se
 sudo localectl --no-convert set-x11-keymap pnx-us-se
