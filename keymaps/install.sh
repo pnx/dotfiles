558046dcafff715ec47eb606c3230ab88b602205
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-07-21 05:59:44 +0200

    keymaps/install.sh: symlink xkb instead of installing in /usr/share

--- keymaps/install.sh
+++ keymaps/install.sh
@@ -2,7 +2,7 @@
 BASE_PATH=$(dirname $(readlink -f $BASH_SOURCE))
 
 cat $BASE_PATH/pnx-us-se.map | gzip | sudo tee /usr/share/kbd/keymaps/i386/qwerty/pnx-us-se.map.gz > /dev/null
-sudo install $BASE_PATH/pnx-us-se /usr/share/X11/xkb/symbols
+ln -sfr $(realpath $BASE_PATH/../xkb) $HOME/.config/
 
 sudo localectl --no-convert set-keymap pnx-us-se
 sudo localectl --no-convert set-x11-keymap pnx-us-se
