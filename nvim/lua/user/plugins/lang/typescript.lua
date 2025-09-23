ed76e46d4153952b09698bd90532a59beb82fe24
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-09-23 19:25:45 +0200

    nvim/lua/user/plugins/lang/typescript.lua: use vue_ls as plugin

--- nvim/lua/user/plugins/lang/typescript.lua
+++ nvim/lua/user/plugins/lang/typescript.lua
@@ -1,5 +1,22 @@
 local lspservers = {
-    ts_ls = {},
+    ts_ls = function()
+        local vue_language_server_path = vim.fn.expand '$MASON/packages' .. '/vue-language-server' .. '/node_modules/@vue/language-server'
+
+        local vue_plugin = {
+            name = '@vue/typescript-plugin',
+            location = vue_language_server_path,
+            languages = { 'vue' },
+            configNamespace = 'typescript',
+        }
+        return {
+            init_options = {
+                plugins = {
+                    vue_plugin,
+                },
+            },
+            filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+        }
+    end,
 }
 
 return {
