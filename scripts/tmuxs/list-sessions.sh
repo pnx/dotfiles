c48615f0aea16a47eb657ffbf2bdeb1d82d0be58
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-10-29 02:07:34 +0100

    scripts/tmuxs/list-sessions.sh: dont list active sessions if we are inside a tmux session

--- scripts/tmuxs/list-sessions.sh
+++ scripts/tmuxs/list-sessions.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# if [[ ! -z $TMUX ]]; then 
-#     tmux list-sessions -F '#{?session_attached,,#{session_name}}' | grep '\S'
-# else
-    tmux list-sessions -F '#{session_name}'
-# fi
+OPTS="list-sessions -F #{session_name}"
+
+## If we are inside tmux already, don't list active sessions
+if [[ ! -z $TMUX ]]; then 
+    OPTS="${OPTS} -f #{==:#{session_attached},0}"
+fi
+
+tmux ${OPTS}
 
