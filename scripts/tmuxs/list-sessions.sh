5b67d85598497b4266dbf3c364a526b0440663f0
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-11-02 07:00:55 +0100

    scripts/tmuxs/list-sessions.sh: improve command with sorting and showing session id

--- scripts/tmuxs/list-sessions.sh
+++ scripts/tmuxs/list-sessions.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-OPTS="list-sessions -F #{session_name}"
+# include session_activity to use for sorting
+OPTS="list-sessions -F #{session_activity},#{session_id},#{session_name}"
 
 ## If we are inside tmux already, don't list active sessions
-if [[ ! -z $TMUX ]]; then 
-    OPTS="${OPTS} -f #{==:#{session_attached},0}"
-fi
+# if [[ ! -z $TMUX ]]; then 
+#     OPTS="${OPTS} -f #{==:#{session_attached},0}"
+# fi
 
-tmux ${OPTS}
+tmux ${OPTS} | sort -nr | cut -d, -f2,3 | sed -r 's/^\$([[:digit:]]+),/\1: /'
 
