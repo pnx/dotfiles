afdfe3780205ac9bf9ea65a7ac533d8270387e6e
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-12-06 08:18:19 +0100

    move sail fish alias to a real bash script.
    and use docker-compose as fallback

--- fish/functions/sail.fish
+++
@@ -1,3 +0,0 @@
-function sail --wraps='[ -f sail ] && sh sail || sh vendor/bin/sail' --description 'alias sail=[ -f sail ] && sh sail || sh vendor/bin/sail'
-    [ -f sail ] && sh sail || sh vendor/bin/sail $argv
-end
