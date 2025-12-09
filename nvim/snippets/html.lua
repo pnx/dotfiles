f153382cb417e98e3400e9f0249bbcf98a360d10
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-12-09 02:04:10 +0100

    add nvim/snippets/html.lua

---
+++ nvim/snippets/html.lua
@@ -0,0 +1,15 @@
+return {
+    s("boilerplate", t({
+        '<html lang="en">',
+        '<head>',
+        '   <meta charset="UTF-8">',
+        '   <meta http-equiv="X-UA-Compatible" content="ie=edge">',
+        '   <title>Title</title>',
+        '   <link rel="stylesheet" href="style.css">',
+        '</head>',
+        '<body>',
+        '   <script src="index.js"></script>',
+        '</body>',
+        '</html>',
+    }))
+}
