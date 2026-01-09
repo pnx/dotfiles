ae9d18c07983f94503b14312e599b0b4d7f8d700
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2026-01-09 21:33:53 +0100

    install.sh: dont link but copy Xresources if file does not exist.
    makes it easier to customize from system to system (if needed)

--- install.sh
+++ install.sh
@@ -21,12 +21,15 @@ done
 $LN $BASE_PATH/bash/bashrc $HOME/.bashrc
 $LN $BASE_PATH/bash/bashrc.d/ $HOME/.bashrc.d
 $LN $BASE_PATH/tmux/config.conf $HOME/.tmux.conf
-$LN $BASE_PATH/Xresources $HOME/.Xresources
 $LN $BASE_PATH/xinitrc $HOME/.xinitrc
 $LN $BASE_PATH/picom.conf $HOME/.config/picom.conf
 $LN $BASE_PATH/fzfrc $HOME/.config/fzfrc
 $LN $BASE_PATH/git/config $HOME/.gitconfig
 $LN $BASE_PATH/git/conf.d/ $HOME/.config/git
+
+if [ ! -f "$HOME/.Xresources" ]; then
+    cp $BASE_PATH/Xresources $HOME/.Xresources
+fi
 
 mkdir -p $HOME/bin
 $LN $BASE_PATH/scripts/tmuxs/main.sh $HOME/bin/tmuxs
