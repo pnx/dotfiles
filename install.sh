10eadcb30b7ed5e2869e71024d2dc6e1d9229aa8
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2026-01-09 21:07:03 +0100

    add xinitrc

--- install.sh
+++ install.sh
@@ -29,6 +29,7 @@ $LN $BASE_PATH/bash/bashrc $HOME/.bashrc
 $LN $BASE_PATH/bash/bashrc.d/ $HOME/.bashrc.d
 $LN $BASE_PATH/tmux/config.conf $HOME/.tmux.conf
 $LN $BASE_PATH/Xresources $HOME/.Xresources
+$LN $BASE_PATH/xinitrc $HOME/.xinitrc
 $LN $BASE_PATH/picom.conf $HOME/.config/picom.conf
 $LN $BASE_PATH/fzfrc $HOME/.config/fzfrc
 $LN $BASE_PATH/git/config $HOME/.gitconfig
