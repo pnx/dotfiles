7cabed990e3b2a7fff079463980ef550760c1efa
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2026-01-11 10:54:35 +0100

    xrandr.sh: update

--- xrandr.sh
+++ xrandr.sh
@@ -8,7 +8,6 @@
 #     --output HDMI-1-0 --primary --mode 3440x1440 --pos 0x0 --rotate normal \
 #     --output DP-1-4 --off
 
-xrandr --output DVI-I-1 --off \
-    --output DP-3 --off \
-    --output HDMI-1 --mode 1920x1080 --pos 760x0 --rotate normal \
-    --output DP-1 --off --output DP-2 --primary --mode 3440x1440 --pos 0x1080 --rotate normal
+xrandr \
+    --output HDMI-0 --mode 1920x1080 --pos 760x0 --rotate normal \
+    --output DP-2 --primary --mode 3440x1440 --pos 0x1080 --rotate normal
