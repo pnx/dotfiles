f17db99444b208ccb2a6a8e32ae51d6fbd969173
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2026-02-16 13:00:21 +0100

    nvim/lua/user/utils/mason.lua: add binary function

--- nvim/lua/user/utils/mason.lua
+++ nvim/lua/user/utils/mason.lua
@@ -1,6 +1,12 @@
 
 local M = {}
 
+local basePath = vim.fn.stdpath("data") .. "/mason/bin/"
+
+function M.binary(name)
+    return basePath .. name
+end
+
 function M.ensure_installed(names)
     return {
         "mason-org/mason.nvim",
