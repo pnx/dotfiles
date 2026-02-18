051f7b199a91961f6ae40351665de89bb35efb2d
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2026-02-18 08:44:03 +0100

    keymaps/install.sh: update

--- keymaps/install.sh
+++ keymaps/install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 BASE_PATH=$(dirname $(readlink -f $BASH_SOURCE))
 
-cat $BASE_PATH/pnx-us-se.map | gzip | sudo tee /usr/share/kbd/keymaps/i386/qwerty/pnx-us-se.map.gz > /dev/null
+VC_DIR=/usr/share/kbd/keymaps/i386/qwerty
+if [ ! -d "$VC_DIR" ];then
+    VC_DIR=/usr/lib/kbd/keymaps/xkb
+fi
+
+cat $BASE_PATH/pnx-us-se.map | gzip | sudo tee ${VC_DIR}/pnx-us-se.map.gz > /dev/null
 ln -sfr $(realpath $BASE_PATH/../xkb) $HOME/.config/
 
-sudo localectl --no-convert set-keymap pnx-us-se
 sudo localectl --no-convert set-x11-keymap pnx-us-se
+sudo localectl --no-convert set-keymap pnx-us-se
