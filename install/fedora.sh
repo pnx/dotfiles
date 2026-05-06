1165ca99fd079bf0042132dd063623e567f7f8a5
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2026-05-06 23:23:54 +0200

    install/fedora.sh: install brave browser

--- install/fedora.sh
+++ install/fedora.sh
@@ -9,12 +9,16 @@ fi
 for sel in "${SELECTED[@]}"; do
     case $sel in
         "core")
+            sudo dnf install -y dnf-plugins-core
+            sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+
             sudo dnf install -y \
                 fish \
                 alacritty \
                 tmux \
                 xdg-utils \
                 xsel \
+                brave-browser \
                 fzf
             ;;
         "nvim")
