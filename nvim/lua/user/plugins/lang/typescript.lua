f5ed798e466f09fbcbf2192c87c5461c50561b52
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2026-05-18 22:14:51 +0200

    nvim/lua/user/plugins/lang/typescript.lua: add tsx treesitter parser

--- nvim/lua/user/plugins/lang/typescript.lua
+++ nvim/lua/user/plugins/lang/typescript.lua
@@ -23,7 +23,7 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         opts = {
-            install = { "typescript" }
+            install = { "typescript", "tsx" }
         }
     },
     {
