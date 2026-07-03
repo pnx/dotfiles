ae02575277508c4ebe1c16bee9fdfab75dc18b8d
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2026-07-03 17:34:47 +0200

    nvim: collapse use statements folds on file open for php.

--- nvim/lua/user/plugins/editor/ufo.lua
+++ nvim/lua/user/plugins/editor/ufo.lua
@@ -4,5 +4,15 @@ return {
     dependencies = {
         'kevinhwang91/promise-async',
     },
-    opts = {}
+    opts = {
+        close_fold_kinds_for_ft = {
+            php = {'namespace_use_declaration'}
+        },
+        provider_selector = function(bufnr, filetype, buftype)
+            _ = bufnr
+            _ = filetype
+            _ = buftype
+            return {'treesitter', 'indent'}
+        end
+    }
 }
